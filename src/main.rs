74a56c861b7950c97ef6d009dfe9e4d329bddc1a
Wandera Jeff <wanderaotieno321@gmail.com>
2026-05-04 12:32:50 +0000

    add tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+
+use std::net::udpSocket;
+#[tokio::main]
+
+async fn main() {
+
+    let socket = udpSocket::bind("0.0.0.0:0").unwrap()
+}
+
+
+async fn build_packet() {
+    /// This fn builds a dns packet
+
+
+}
+
+async fn parse(domain: &str) {
+
 }
